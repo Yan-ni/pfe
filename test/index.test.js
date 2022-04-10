ac40824caeae841bf75246182e2cd1e362c4d6f7
Anis <anis01000001@gmail.com>
2022-04-10 15:40:43 +0100

    feat: add test

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,52 @@
 const request = require('supertest');
 const app = require('../app');
 
+
+beforeAll(async () => {
+  await app.startDatabase();
+});
+
+
 describe('GET /api', () => {
+  test('GET /api/fournisseur', async () => {
+    const response = await request(app).get('/api/fournisseur');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
+  });
+  
+  test('GET /api/technicien', async () => {
+    const response = await request(app).get('/api/technicien');
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
+  });
+
+  test('GET /api/equipement', async () => {
+    const response = await request(app).get('/api/equipement');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
+  });
+
+  test('GET /api/plateforme', async () => {
+    const response = await request(app).get('/api/plateforme');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
+  });
+
   test('GET /api/contrat', async () => {
-    await app.startDatabase();
     const response = await request(app).get('/api/contrat');
 
     expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
   });
-});
+
+  test('GET /api/ticket', async () => {
+    const response = await request(app).get('/api/ticket');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch('application/json');
+  });
+});    
