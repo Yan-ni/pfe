6437df6cc3a916c83dcaf203b72ce041acb3bbc8
Anis <anis01000001@gmail.com>
2022-04-11 17:36:40 +0100

    implement side bar navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,25 @@ import "./assets/styles/style.css";
 import { Routes, Route } from "react-router-dom";
 import { Fournisseur, Technicien, Equipement, Contrat, PlateForm, Ticket } from './pages';
 import { APIStoreProvider } from "./APIStoreContext";
+import SideBarNav from "./components/SideBarNav";
 
 function App() {
   return (
     <div className="App">
       <APIStoreProvider>
+        <SideBarNav />
         <Routes>
           <Route path="/" element={<Fournisseur />} />
 
-          <Route path="/Technicien" element={<Technicien />} />
+          <Route path="/techniciens" element={<Technicien />} />
 
-          <Route path="/Equipement" element={<Equipement />} />
+          <Route path="/equipements" element={<Equipement />} />
 
-          <Route path="/Contrat" element={<Contrat />} />
+          <Route path="/contrats" element={<Contrat />} />
 
-          <Route path="/PlateForm" element={<PlateForm />} />
+          <Route path="/plateformes" element={<PlateForm />} />
 
-          <Route path="/Ticket" element={<Ticket />} />
-
+          <Route path="/tickets" element={<Ticket />} />
         </Routes>
       </APIStoreProvider>
     </div>
