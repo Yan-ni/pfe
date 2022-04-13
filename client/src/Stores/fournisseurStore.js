5b9a701a2639bc7a7581b4c4ed4cd495f54d199b
Anis <anis01000001@gmail.com>
2022-04-13 16:39:45 +0100

    make specific store for fournisseur page

---
+++ client/src/Stores/fournisseurStore.js
@@ -0,0 +1,37 @@
+import { makeObservable, observable, action } from 'mobx';
+import axios from 'axios';
+
+class FournisseurStore {
+  constructor() {
+    makeObservable(this, {
+      fournisseurs: observable,
+      setFournisseurs: action,
+      loadFournisseurs: action,
+      addFournisseur: action
+    });
+  }
+
+  fournisseurs = [];
+
+  addFournisseur(fournisseur) {
+    return axios
+      .post('/api/fournisseur', fournisseur)
+      .then(({data}) => {
+        this.setFournisseurs([...this.fournisseurs, data])
+      })
+      .then(error => console.error(error));
+  }
+
+  setFournisseurs(fournisseurs) {
+    this.fournisseurs = fournisseurs;
+  }
+
+  async loadFournisseurs() {
+    await axios
+      .get('/api/fournisseur')
+      .then(({data}) => this.setFournisseurs(data))
+      .catch(error => console.error(error));
+  }
+}
+
+export default new FournisseurStore();
