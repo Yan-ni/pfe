3516c94d77aa4c62d8bf017203cb346eca3ed45b
Anis <anis01000001@gmail.com>
2022-04-13 16:40:46 +0100

    implement add fournisseur modal functionality

--- client/src/pages/Fournisseur.jsx
+++ client/src/pages/Fournisseur.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
-import Button from "../components/basicComponents/Button";
+import React, { useState, useContext, useEffect } from "react";
+import { Button } from "../components/basicComponents";
 import AddFournisseur from "../components/fournisseur/AddFournisseur";
 import "../assets/styles/fournisseur.css";
+import { observer } from "mobx-react";
+import { APIStoreContext } from "../APIStoreContext";
 
 function Fournisseur() {
   const [addFournisseurModalDisplay, setAddFournisseurModalDisplay] = useState(false);
+
+  const { fournisseurStore } = useContext(APIStoreContext);
+
+  useEffect(() => {
+    fournisseurStore.loadFournisseurs();
+  }, []);
 
   return (
     <div className="fournisseur">
       <h1>Fournisseur</h1>
       <Button onClick={() => setAddFournisseurModalDisplay(true)}>Ajouter un fournisseur</Button>
 
-      <AddFournisseur addFournisseurModalDisplay={addFournisseurModalDisplay} setAddFournisseurModalDisplay={setAddFournisseurModalDisplay} />
+      <AddFournisseur
+        addFournisseurModalDisplay={addFournisseurModalDisplay}
+        setAddFournisseurModalDisplay={setAddFournisseurModalDisplay} />
     </div>
   );
 }
 
-export default Fournisseur;
+export default observer(Fournisseur);
